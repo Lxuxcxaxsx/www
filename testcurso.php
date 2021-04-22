9caa7d2c79d537b001edebe86e70e041decf0c0a
Lxuxcxaxsx <esau.mdor@hotmail.com>
2021-04-21 22:35:26 -0500

    Mi primer commit

---
+++ testcurso.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Curso</title>
+</head>
+<body>
+<?php
+/*RECORDATORIO/NOTA ------------------------------------------------------------------------------------------------------
+    En producción, tus páginas web serán una combinación de PHP, HTML, JavaScript
+    y algunas sentencias MySQL, y se diseñarán con CSS.
+    
+    Por ahora concentrate en escribir código PHP de manera que obtengas el resultado que esperas,
+    o al menos que entiendas el resultado que realmente obtienes.
+------------------------------------------------------------------------------------------------------------------------*/
+    //Ejemplo 3-1. Llamada de PHP.
+    
+//------------------------------------------------------------------------------------------------------------------------
+    //Ejemplo 
+?>   
+</body>
+</html>
---
+++ testlibro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Libro</title>
+</head>
+<body>
+<?php
+/*RECORDATORIO/NOTA ------------------------------------------------------------------------------------------------------
+    En producción, tus páginas web serán una combinación de PHP, HTML, JavaScript
+    y algunas sentencias MySQL, y se diseñarán con CSS.
+    
+    Por ahora concentrate en escribir código PHP de manera que obtengas el resultado que esperas,
+    o al menos que entiendas el resultado que realmente obtienes.
+------------------------------------------------------------------------------------------------------------------------*/
+    //Ejemplo 3-1. Llamada de PHP.
+    echo "Hola mundo";
+//------------------------------------------------------------------------------------------------------------------------
+    //Ejemplo 3-3. Tres clases diferentes de asignación de variables, siempre acompañadas por el símbolo $
+    $counter = 1; //Números
+    $string = "Hola mundo"; //Cadenas (caracteres)
+    $array = arrary("uno", "dos", "tres"); //Matrices
+//------------------------------------------------------------------------------------------------------------------------
+      //Ejemplo 
+?>   
+</body>
+</html>
